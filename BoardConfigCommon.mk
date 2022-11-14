2db770b1ff9bc4cf42c91aed8e7774a45de1f4cc
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    universal9611: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,6 +100,7 @@ TARGET_USES_VULKAN := true
 ## Kernel
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_NO_GCC := true
 
 ## Keymaster
 TARGET_KEYMASTER_VARIANT := samsung
